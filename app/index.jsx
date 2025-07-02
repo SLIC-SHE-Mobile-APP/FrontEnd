1266b473a339c3313042aca50887d4cd6f4d55cf
udhantha20010630 <yukiariyaratna@gmail.com>
2025-07-02 20:33:45 +0530

    02/07/2025 -> 8.33p.m.

--- app/index.jsx
+++ app/index.jsx
@@ -2,11 +2,12 @@ import { StyleSheet } from 'react-native';
 
 import Home from './home.jsx';
 import LoginRequestOTP from './loginRequestOTP.jsx';
+import OTPVerificationBank from './OTPVerificationBank.jsx';
 
 
 export default function HomeScreen() {
   return (
-    <LoginRequestOTP/>
+    <Home/>
   );
 }
 
